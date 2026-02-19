cb64ad45c747decc127b61bd205550d06dd39505
developershahzod <114401738+developershahzod@users.noreply.github.com>
2026-02-20 02:50:01 +0500

    Fix: add provider import to car_wash_detail_screen_new.dart for context.read<>()

--- flutter_app/lib/screens/car_wash/car_wash_detail_screen_new.dart
+++ flutter_app/lib/screens/car_wash/car_wash_detail_screen_new.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../config/app_theme.dart';
 import '../../services/full_api_service.dart';
